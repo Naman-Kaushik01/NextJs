7e3e11d8b8631026b70c3251f772919a5d6dee40
user.name="Naman Kaushik <user.email = "kaushiknaman68@gmail.com>
2026-08-23 23:20:57 +0530

    Initialized first app

---
+++ my-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Card from "@/components/Card";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Card />
+    </div>
+  );
+}
---
+++ my-app/components/Card.tsx
@@ -0,0 +1,8 @@
+export default function Card(){
+    return(
+        <div>
+            <h1>Card Component</h1>
+            <p>This is a simple card Component </p>
+        </div>
+    )
+}
