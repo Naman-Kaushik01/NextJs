f9df0ee91dca1e8cd15bf6fce0bc534b91f75ba1
user.name="Naman Kaushik <user.email = "kaushiknaman68@gmail.com>
2026-08-24 09:58:17 +0530

    created nested route

---
+++ my-app/app/about/sharda/page.tsx
@@ -0,0 +1,10 @@
+export default function Sharda() {
+    return (
+       <>
+        <div>
+            <h1>Sharda Page</h1>
+            <p>This is Sharda Page of application</p>
+        </div>
+       </>
+    );
+}
