27dc9556fc560ee2584100d495f80532026ed486
user.name="Naman Kaushik <user.email = "kaushiknaman68@gmail.com>
2026-08-24 22:43:41 +0530

    api called

--- my-app/app/sharda/classroom/page.tsx
+++ my-app/app/sharda/classroom/page.tsx
@@ -1,10 +1,23 @@
-export default function Classroom() {
+type User={
+    id: number;
+    name: string;
+    username: string;
+}
+export default async function Classroom() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    
     return (
        <>
-        <div>
+        <main>
             <h1>Classroom Page</h1>
             <p>This is Classroom Page of application</p>
-        </div>
+            <ul>
+                {users.map((user : User) =>(
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </main>
        </>
     );
 }
