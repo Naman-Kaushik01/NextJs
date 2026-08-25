06239671b53368bcc1a63be322784b2fa621f3a2
user.name="Naman Kaushik <user.email = "kaushiknaman68@gmail.com>
2026-08-25 12:35:46 +0530

    understood serverActions

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Card from "@/components/Card";
-
+import { someActions } from "@/app/actions/somethins";
 export default function Home() {
   return (
     <div>
       <h1>Hello World</h1>
       <Card title="Card 1" description="This is the first Card"/>
+      <button onClick={someActions}>clickMe</button>
     </div>
   );
 }
